4900cfc4ff44d861274c94c85100fca62593bd2c
Urlugall <urlugall@gmail.com>
2026-01-08 17:09:27 +0100

    github update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "slize-front";
+
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  images: { unoptimized: true },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
